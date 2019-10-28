dab849a50f6ba68edcecfa00834e2ee2c9c9d32f
ishaq4466 <shaikhishaq1996@gmail.com>
2019-10-28 18:53:58 +0000

    modified calcuate script

--- calculate.sh
+++ calculate.sh
@@ -9,23 +9,29 @@ calculate_diff()
 	echo "starttime: $1"	
 	echo "endtime: $2"
 	diff=$(( $(date -d "$2" "+%s") - $(date -d "$1" "+%s") ))
-	echo "scale=2 ; $diff/60" | bc  
+	echo "scale=2 ; $diff/3600" | bc  
 	#echo "timediff:$diff"
 }
 
 
 starttime=$(date "+%H:%S")
-echo $starttime
+echo $starttime 
 
-i=2
-while [ $i -ne 20 ] 
+SECONDS=0 # add this 
+
+i=1
+while [ $i -ne 6 ] 
 do
 echo "$(date "+%H:%S")=>in while"
 sleep 1
 i=$((i+1))
 done
-endtime=$(date "+%H:%S")
-calculate_diff $starttime $endtime
+
+durationInSeconds=$SECONDS # add this
+echo "Total time taken: $(($durationInSeconds / 60)) minutes and $(($durationInSeconds % 60)) seconds."  # add this
+
+#endtime=$(date "+%H:%S")
+#calculate_diff $starttime $endtime
 
 
 
