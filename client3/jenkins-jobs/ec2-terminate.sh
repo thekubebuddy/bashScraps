56e4d8054cd36b03042a59b9af8f4cd664db4200
ishaq4466 <smartbuddy1995@gmail.com>
2019-10-29 18:50:26 +0530

    Added a jenkins job for ec2-termination

---
+++ client3/jenkins-jobs/ec2-terminate.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source parameter.config
+
+echo "********** EC2 state change job triggered *************"
+#region=ap-south-1
+echo "Instance Id: $instance_id "
+echo "Instance state: $instance_state"
+echo "region: $region"
+
+case $instance_state in
+"start")
+echo "Starting the instance as per parameter"
+echo aws ec2 $instance_state --instance-id $instance_id --region $region
+--dry-run
+
+;;
+"stop" | "Stop")
+echo "stoping the instance as per parameter"
+echo aws ec2 $instance_state --instance-id $instance_id --region $region
+;;
+
+"terminate" | "Terminate")
+echo "Terminating the instance as per parameter"
+echo aws ec2 $instance_state --instance-id $instance_id --region $region
+--dry-run
+;;
+*)
+echo "unknown state"
+;;
+esac
+echo "************  Job ends  ***************"
